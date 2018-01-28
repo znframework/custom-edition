5935cc8a318ca76ec05ce68e98e3e50f68716b51
Ozan UYKUN <znframework@gmail.com>
2018-01-28 16:54:10 +0300

    Added Package

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * ZN PHP Web Framework
+ * 
+ * "Simplicity is the ultimate sophistication." ~ Da Vinci
+ * 
+ * @package ZN
+ * @license MIT [http://opensource.org/licenses/MIT]
+ * @author  Ozan UYKUN [ozan@znframework.com]
+ * @since   2011
+ */
+
+/*
+|--------------------------------------------------------------------------
+| Require Core File
+|--------------------------------------------------------------------------
+|
+| It includes the necessary things for the operation of the system.
+|
+*/
+
+require __DIR__ . '/vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run ZN
+|--------------------------------------------------------------------------
+|
+| Simplicity is our principle. Enjoy it.
+|
+*/
+
+ZN\ZN::defines
+([
+    #'MODELS_DIR'      => '',
+    #'CONTROLLERS_DIR' => '',
+    #'VIEWS_DIR'       => '',
+    #'ROUTES_DIR'      => '',
+    #'CONFIG_DIR'      => '',
+    #'DATABASES_DIR'   => '',
+    #'STORAGE_DIR'     => '',
+    #'COMMANDS_DIR'    => '',
+    #'LANGUAGES_DIR'   => '',
+    #'LIBRARIES_DIR'   => '',
+    #'AUTOLOAD_DIR'    => '',
+    #'LAYERS_DIR'      => '',
+    #'FILES_DIR'       => '',
+    #'TEMPLATES_DIR'   => '',
+    #'THEMES_DIR'      => '',
+    #'PLUGINS_DIR'     => '',
+    #'UPLOADS_DIR'     => '',
+
+])::run('CE');
