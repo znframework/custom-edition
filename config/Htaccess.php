4be1400fa7a40581a0d2e5d9862079b86ff130bf
ozanuykun <ozanbote@gmail.com>
2021-02-27 16:57:23 +0300

    Changed Settings\Htaccess.php config.

--- config/Htaccess.php
+++ config/Htaccess.php
@@ -58,7 +58,8 @@
     'settings' =>
     [
         'IfModule mod_headers.c'                              => ['Options -Indexes'],
-	    'FilesMatch "^(?i:docker\-compose\.yml|Dockerfile)$"' => ['deny from all']
+	    'FilesMatch "^(?i:docker\-compose\.yml|Dockerfile)$"' => ['deny from all'],
+        'RedirectMatch 403 /(config)/.*$'
 
         //'IfModule mod_rewrite.c'  =>
         //[
